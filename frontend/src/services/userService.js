ec76048e60326672fb0cbba3264245f2c3e9a589
Nguyen Cong Huy <nguyenconghuy62hb@gmail.com>
2026-06-27 17:36:36 +0700

    feat: conflict issue 45

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,6 +1,16 @@
 import api from './api'
 
 export const userService = {
+  async getMe() {
+    const res = await api.get('/users/me')
+    return res.data
+  },
+
+  async updateMe(payload) {
+    const res = await api.patch('/users/me', payload)
+    return res.data
+  },
+
   async getUsers() {
     const res = await api.get('/users')
     return res.data
